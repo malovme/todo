3277a1571963d8842bc9e346fc0802c72f2df598
Michael Malov <mmeemail@gmail.com>
2017-08-11 20:47:25 +0300

    resque path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Resque::Server, :at => "/resque"
+
   resources :lists, only: [:create, :destroy] do
     resources :tasks, except: [:new, :edit, :show]
   end
