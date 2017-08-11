56b4e7225a859f23124a15993dad38cbebb25324
Michael Malov <mmeemail@gmail.com>
2017-08-11 23:51:57 +0300

    solve deprecated warning with node

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,4 @@
-if node['dna']['name'] == 'resque'
+if config.node['dna']['name'] == 'resque'
   worker_count = 2
   worker_count.times do |count|
     run "sudo monit restart resque_todo_V5_#{count}"
