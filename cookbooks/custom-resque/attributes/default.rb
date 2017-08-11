44d48f68209034aef5c9d9e815c33a417d035896
Michael Malov <mmeemail@gmail.com>
2017-08-11 22:57:38 +0300

    set worker to 2

--- cookbooks/custom-resque/attributes/default.rb
+++ cookbooks/custom-resque/attributes/default.rb
@@ -1,3 +1,3 @@
 # default['resque']['is_resque_instance'] = (node['dna']['instance_role'] == 'util' && node['dna']['name'] == 'resque')
 # default['resque']['applications'] = %w[todo]
-# default['resque']['worker_count'] = 4
+default['resque']['worker_count'] = 2
